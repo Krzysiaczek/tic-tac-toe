e94dbb2af005847f0f050d93d5f2f1c81cfadbf5
Krzysiaczek <krzysiaczek@icloud.com>
2023-06-12 09:08:39 +0100

    Change titles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @stack('meta')
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Tic, tac, toe') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
